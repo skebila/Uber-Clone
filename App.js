70b20a34ed77af67c4b12ada0eadf44f95687d40
Steven <bongamke@sheridancollege.ca>
2021-10-28 21:10:18 -0400

    slices.js

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+//1. setup redux
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={}>
+      <View style={styles.container}>
+        <Text>Let's build Uber!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+    
   );
 }
 
